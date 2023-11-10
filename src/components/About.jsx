92fcd3f4535ba255290231cfa31f55e2111c1765
aniruddhparwal <aniruddhparwal@gmail.com>
2023-11-10 12:55:43 +0530

    add website

---
+++ src/components/About.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Something</p>
+        <h2 className={styles.sectionHeadText}>About Me</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        My journey in Information Technology has seen me evolve into a dynamic professional adept at navigating the ever-evolving landscape of web development. Proficient in JavaScript, Node.js, MySQL, MongoDB, and React.js, I specialize in crafting dynamic and interactive applications, ensuring robust back-end solutions for efficient data management. My expertise extends to system design principles, reflecting my commitment to continuous learning and a desire to architect solutions that transcend the ordinary. With a collaborative spirit evident in my GitHub contributions.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,303 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  visit,
+  hackerEarth,
+  firstSense,
+  threejs,
+  mrcto,
+  technlogi,
+  youtubeClone,
+  faceBookClone,
+  amazonClone,
+  starMatch,
+  schoolWebsite,
+  restaurantFinder,
+  covid19,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "System Design",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Full-stack Developer SDE",
+    company_name: "Visit Health",
+    date: "May 2023 - Present",
+    icon: visit,
+    iconBg: "#E6DEDD",
+    points: [
+      "Developed reward system for a leading insurance company with Node.js, serving 8 million users.",
+      "Implemented custom payment service using Razorpay’s APIs for diverse payment methods.",
+      "Pioneered creation of AI-powered health platform from scratch, benefiting users in 5 countries.",
+      "Collaborated within cross-functional teams to develop feature-rich web applications.",
+      "Leveraged JavaScript, React.js, and Redux to architect dynamic front-end interfaces.",
+    ],
+  },
+  {
+    title: "Full Stack Associate",
+    company_name: "HackerEarth",
+    icon: hackerEarth,
+    iconBg: "#E6DEDD",
+    date: "July 2022 - May 2023",
+    points: [
+      "Led team of 4-5 interns, providing guidance, mentorship, and fostering collaborative learning.",
+      "Participated actively in client calls, contributing insights and aligning development efforts.",
+      "Demonstrated exceptional problem-solving skills, addressing on-call issues with minimal disruption.",
+      "Leveraged React.js, Node.js, and MongoDB to architect and develop robust web applications.",
+      "Championed use of Jest for testing, ensuring quality and reliability of codebase.",
+    ],
+  },
+  {
+    title: "Full Stack Intern",
+    company_name: "HackerEarth",
+    icon: hackerEarth,
+    iconBg: "#E6DEDD",
+    date: "January 2022 - July 2022",
+    points: [
+      "Gained hands-on experience in full-stack development, working with React.js and Node.js.",
+      "Engaged in immersive projects fostering deep understanding of application architecture.",
+      "Leveraged Selenium for automated testing, maintaining code quality and ensuring reliability.",
+      "Active interest in DevOps practices, contributing to continuous integration and deployment workflows.",
+      "Contributed to RESTful APIs, optimizing data retrieval and manipulation for efficient functionality.",
+    ],
+  },
+  {
+    title: "Front-End Facilitator",
+    company_name: "Facebook Developer Circle Indore",
+    date: "November 2019 - May 2023",
+    icon: meta,
+    iconBg: "#383E56",
+    points: [
+      "Recognized by Facebook team as a facilitator for their distinguished learning program.",
+      "Served as facilitator, guiding participants through learning journey in Frontend Web Technology.",
+      "Organized and led StudyTime workshops, fostering progress and addressing obstacles.",
+      "Contributed as an organizer for numerous weekend events, enriching local developer community.",
+      "Focused on creating engaging experiences that spanned various tech-related topics.",
+    ],
+  },
+  {
+    title: "Frontend Developer",
+    company_name: "Firstsense Technology",
+    date: "July 2021 - January 2022",
+    icon: firstSense,
+    iconBg: "#E6DEDD",
+    points: [
+      "Led TypeScript and React.js revamp for a user-friendly dashboard interface.",
+      "Collaborated with design for seamless translation of complex mockups.",
+      "Implemented responsive design and integrated TensorFlow.js for ML-driven features.",
+      "Utilized Three.js for immersive 3D visualizations, enhancing aesthetics.",
+      "Demonstrated expertise in performance optimization and fostered a culture of learning through documentation.",
+    ],
+  },
+  {
+    title: "Software Developer Engineer Trainee",
+    company_name: "mr. CTO",
+    date: "March 2021 - September 2021",
+    icon: mrcto,
+    iconBg: "#E6DEDD",
+    points: [
+      "Worked on diverse software projects, gaining hands-on experience in software development life cycle.",
+      "Collaborated with senior developers to contribute code enhancements and bug fixes.",
+      "Gained proficiency in programming languages, data structures, and algorithms.",
+      "Participated in code reviews and learned to follow best practices for coding standards.",
+    ],
+  },
+  {
+    title: "Web Development Intern",
+    company_name: "Technlogi Solution",
+    date: "June 2020 - November 2020",
+    icon: technlogi,
+    iconBg: "#383E56",
+    points: [
+      "Developed and maintained web pages, ensuring consistent design and functionality.",
+      "Acquired practical experience in front-end technologies such as HTML, CSS, and JavaScript.",
+      "Contributed to the team's development efforts, supporting the creation of web-based solutions.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Aniruddh proved me wrong.",
+    name: "Pradhyum Vyas",
+    designation: "SDE",
+    company: "Accenture",
+    image: "https://avatars.githubusercontent.com/u/38814603?v=4",
+  },
+  {
+    testimonial:
+      "After Aniruddh optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Harshdeep Yadav",
+    designation: "Frontend Developer",
+    company: "Trendlyne",
+    image:
+      "https://media.licdn.com/dms/image/C5603AQHtvKvBi4BLRA/profile-displayphoto-shrink_400_400/0/1662899860575?e=1704931200&v=beta&t=f9yGL1LUeZ8IHwUaWjAVIxuDZiuyMB4caamdIzEudUw",
+  },
+];
+
+const projects = [
+  {
+    name: "Youtube Clone",
+    description:
+      "This Clone website is built using React-JS. Also used SCSS and Bem Naming Convention in the development.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "flexbox",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: youtubeClone,
+    source_code_link: "https://github.com/aniruddhparwal/youtube-clone",
+  },
+  {
+    name: "COVID-19 Tracker",
+    description:
+      "This website is built using React-JS and used open diseases.sh API for data. Also used interactive Graph for represent data.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: covid19,
+    source_code_link: "https://www.github.com/aniruddhparwal/COVID19-Tracker/",
+  },
+  {
+    name: "Amazon Clone",
+    description:
+      "This is Simple clone website built using React-JS and used FireBase to authenticate user. User can create account and Login.",
+    tags: [
+      {
+        name: "reactJs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "FireBase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: amazonClone,
+    source_code_link: "https://github.com/aniruddhparwal/amazon-clone",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
