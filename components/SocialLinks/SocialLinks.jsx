e8bc976a4b139b71461ff971650b63a9d713959a
aniruddhparwal <aniruddhparwal@gmail.com>
2024-10-24 01:38:24 +0530

    added meta tags and aria-lable

--- components/SocialLinks/SocialLinks.jsx
+++ components/SocialLinks/SocialLinks.jsx
@@ -6,22 +6,22 @@ function SocialLinks() {
     <>
         <ul className="ml-1 mt-8 flex items-center">
             <li className="mr-5 text-xs">
-                <a className="block hover:text-slate-200" href="https://github.com/aniruddhparwal" target="_blank">
+                <a className="block hover:text-slate-200" href="https://github.com/aniruddhparwal" target="_blank" aria-label="Click to visit Github Profile" alt="Github">
                     <GithubIcon style={{height: 20, width: 20,}} />
                 </a>
             </li>
             <li className="mr-5 text-xs">
-                <a className="block hover:text-slate-200" href="https://twitter.com/aniruddhparwal" target="_blank">
+                <a className="block hover:text-slate-200" href="https://twitter.com/aniruddhparwal" target="_blank" aria-label="Click to visit Twitter Profile" alt="Twitter">
                     <TwitterIcon style={{height: 20, width: 20,}} />
                 </a>
             </li>
             <li className="mr-5 text-xs">
-                <a className="block hover:text-slate-200" href="https://www.linkedin.com/in/aniruddhparwal/" target="_blank">
+                <a className="block hover:text-slate-200" href="https://www.linkedin.com/in/aniruddhparwal/" target="_blank" aria-label="Click to visit Linkedin Profile" alt="Linkedin">
                     <LinkedInIcon style={{height: 20, width: 20,}} />
                 </a>
             </li>
             <li className="mr-5 text-xs">
-                <a className="block hover:text-slate-200" href="https://www.instagram.com/aniruddhparwal/" target="_blank">
+                <a className="block hover:text-slate-200" href="https://www.instagram.com/aniruddhparwal/" target="_blank"  aria-label="Click to visit Instagram Profile" alt="Instagram">
                     <InstagramIcon style={{height: 20, width: 20,}} />
                 </a>
             </li>
--- components/layout/layout.jsx
+++ components/layout/layout.jsx
@@ -14,8 +14,29 @@ function PageLayout({children}) {
     <main className={`${inter.className}`}>
         <div>
             <Head>
-                <title>Aniruddh Parwal</title>
-                {/* <link rel="icon" href="/favicon-96x96.png" /> */}
+                <title>Aniruddh Parwal | Full-Stack Developer & System Design Expert</title>
+                <meta name="description" content="Aniruddh Parwal, a Full-Stack Developer and System Design Expert with expertise in MERN stack, AR development, and crafting digital experiences. Currently working as an SDE-2 at Visit Health Pvt. Ltd." />
+                <meta name="keywords" content="Aniruddh Parwal, Full-Stack Developer, System Design, MERN Stack, JavaScript, React, Node.js, AR Developer, Visit Health, Noida, Software Engineer, Digital Experiences" />
+                <meta name="author" content="Aniruddh Parwal" />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+                {/* Open Graph / Facebook */}
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://www.aniruddhparwal.com" />
+                <meta property="og:title" content="Aniruddh Parwal | Full-Stack Developer & System Design Expert" />
+                <meta property="og:description" content="Aniruddh Parwal, Full-Stack Developer with expertise in MERN stack, system design, and digital solutions. Learn more about his projects and career." />
+                <meta property="og:image" content="/aniruddh.jpg" />
+
+                {/* Twitter */}
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content="https://www.aniruddhparwal.com/" />
+                <meta property="twitter:title" content="Aniruddh Parwal | Full-Stack Developer & System Design Expert" />
+                <meta property="twitter:description" content="Learn more about Aniruddh Parwal, Full-Stack Developer, MERN stack expert, and System Design enthusiast. See his latest projects and contributions." />
+                <meta property="twitter:image" content="/aniruddh.jpg" />
+
+                {/* Favicon */}
+                <link rel="icon" href="/aniruddh.jpg" />
             </Head>
 
             <div className="bg-slate-900">
