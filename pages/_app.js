37f175d32be8fd00186d04e9e180ef4150f2be57
aniruddhparwal <aniruddhparwal@gmail.com>
2024-10-23 23:48:15 +0530

    added clarity

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,14 @@
 import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
+import { clarity } from "react-microsoft-clarity";
+import React, { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    console.log("hiii", process.env.NEXT_PUBLIC_clarityId);
+    clarity.init(process.env.NEXT_PUBLIC_clarityId);
+  }, []);
   return (
     <ThemeProvider attribute="class" defaultTheme="light">
       <Component {...pageProps} />
