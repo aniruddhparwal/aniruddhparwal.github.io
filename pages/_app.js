c821fcfeb914ef06916ab71cf430afdd8f1275d6
aniruddhparwal <aniruddhparwal@gmail.com>
2024-11-23 12:20:17 +0530

    Added Google analytics on landing page

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/react";
 import { clarity } from "react-microsoft-clarity";
+import Head from "next/head";
 import React, { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
@@ -12,9 +13,29 @@ export default function App({ Component, pageProps }) {
     clarity.init(process.env.NEXT_PUBLIC_clarityId);
   }, []);
   return (
-    <ThemeProvider attribute="class" defaultTheme="light">
-      <Component {...pageProps} />
-      <Analytics />
-    </ThemeProvider>
+    <>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
+      <ThemeProvider attribute="class" defaultTheme="light">
+        <Component {...pageProps} />
+        <Analytics />
+      </ThemeProvider>
+    </>
   );
 }
